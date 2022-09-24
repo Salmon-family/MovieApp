7d533c3dbf451ca880f3fb9b39185d5d340ab13f
Falah Hassan <devfalahhassan@gmail.com>
2022-09-24 13:05:12 -0700

    add movie service

--- app/src/main/java/com/karrar/movieapp/data/remote/MovieService.kt
+++ app/src/main/java/com/karrar/movieapp/data/remote/MovieService.kt
@@ -1,4 +1,20 @@
 package com.karrar.movieapp.data.remote
 
-class MovieService {
+import com.karrar.movieapp.data.remote.response.MovieResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MovieService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies() : Response<MovieResponse>
+
+    @GET("movie/upcoming")
+    suspend fun getUpcomingMovies() : Response<MovieResponse>
+
+    @GET("movie/top_rated")
+    suspend fun getTopRatedMovies() : Response<MovieResponse>
+
+    @GET("movie/now_playing")
+    suspend fun getNowPlayingMovies() : Response<MovieResponse>
 }
