212ab3de54696129db11cf0e992be891a176019f
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-09-27 22:59:41 +0300

    add event class

---
+++ app/src/main/java/com/karrar/movieapp/utilities/Event.kt
@@ -0,0 +1,29 @@
+package com.karrar.movieapp.utilities
+
+import androidx.lifecycle.Observer
+
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    fun peekContent(): T = content
+}
+
+class EventObserve<T>(private val onEventUnhandledContent:(T) ->Unit)
+    : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>?) {
+        event?.getContentIfNotHandled()?.let {
+            onEventUnhandledContent(it)
+        }
+    }
+}
