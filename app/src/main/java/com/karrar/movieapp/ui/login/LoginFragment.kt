e29fddfd20526084c0a9e22a2171e76534e4ef50
Falah Hassan <devfalahhassan@gmail.com>
2022-09-29 22:27:35 -0700

    handle login view model

--- app/src/main/java/com/karrar/movieapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/karrar/movieapp/ui/login/LoginFragment.kt
@@ -1,59 +1,14 @@
-package com.karrar.movieapp
+package com.karrar.movieapp.ui.login
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.karrar.movieapp.R
+import com.karrar.movieapp.databinding.FragmentLoginBinding
+import com.karrar.movieapp.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@AndroidEntryPoint
+class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+    override val layoutIdFragment = R.layout.fragment_login
+    override val viewModel: LoginViewModel by viewModels ()
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/karrar/movieapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/karrar/movieapp/ui/login/LoginViewModel.kt
@@ -1,4 +1,59 @@
 package com.karrar.movieapp.ui.login
 
-class LoginViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.karrar.movieapp.data.remote.Either
+import com.karrar.movieapp.data.remote.repository.AccountRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val accountRepository: AccountRepository,
+) : ViewModel() {
+
+    private val _userName = MutableLiveData("")
+
+
+    private val _password = MutableLiveData("")
+    fun onPasswordChanged(text: String) {
+        _password.postValue(text)
+    }
+
+    fun onUserNameChanged(text: String) {
+        _userName.postValue(text)
+    }
+
+    fun onClickLogin() {
+        viewModelScope.launch {
+            val loginState = loginWithUserNameANdPassword(_userName.value.toString(), _password.value.toString())
+            when (loginState) {
+                is Either.Left -> onLoginSuccessfully()
+                is Either.Right -> onLongError(loginState.value)
+            }
+
+        }
+    }
+
+    private suspend fun loginWithUserNameANdPassword(
+        userName: String,
+        password: String,
+    ): Either<Boolean, String> {
+        return accountRepository.loginWithUserNameANdPassword(
+            userName,
+            password
+        )
+    }
+
+    private fun onLoginSuccessfully() {
+
+    }
+
+    private fun onLongError(message:String) {
+
+    }
+
+
 }
