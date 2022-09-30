1967bb7fe6ecc79b83fe6ab44498a88d927116da
Falah Hassan <devfalahhassan@gmail.com>
2022-09-29 22:23:32 -0700

    add data class parser

--- app/src/main/java/com/karrar/movieapp/utilities/DataClassParser.kt
+++ app/src/main/java/com/karrar/movieapp/utilities/DataClassParser.kt
@@ -1,4 +1,10 @@
 package com.karrar.movieapp.utilities
 
-class DataClassParser {
+import com.google.gson.Gson
+import javax.inject.Inject
+
+class DataClassParser @Inject constructor(val gson: Gson) {
+    inline fun <reified T> parseFromJson(json: String , classType: Class<T>): T {
+        return gson.fromJson(json,classType)
+    }
 }
