5b66c471d4e6c621899b62711324c2a6fde52dda
alkomity <197023.it.eng@student.aden-univ.net>
2022-10-01 16:35:46 +0300

    add some functions to binding adapter

--- app/src/main/java/com/karrar/movieapp/utilities/BindingAdapter.kt
+++ app/src/main/java/com/karrar/movieapp/utilities/BindingAdapter.kt
@@ -1,14 +1,22 @@
 package com.karrar.movieapp.utilities
 
+import android.view.View
 import android.widget.ImageView
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.GenericTransitionOptions
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.android.material.imageview.ShapeableImageView
 import com.karrar.movieapp.R
 import com.karrar.movieapp.base.BaseAdapter
+import com.karrar.movieapp.data.remote.State
+import com.karrar.movieapp.utilities.Constants.IMAGE_DOWNLOAD_URL
 import com.squareup.picasso.Picasso
 
 @BindingAdapter("app:movieImage")
-fun bindMovieImage(image: ImageView, imageURL: String?) {
+fun bindMovieImage(image: ShapeableImageView, imageURL: String?) {
     imageURL?.let {
         Picasso.get()
             .load(imageURL)
@@ -20,6 +28,29 @@ fun bindMovieImage(image: ImageView, imageURL: String?) {
 @BindingAdapter(value = ["app:items"])
 fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
     (view.adapter as BaseAdapter<T>?)?.setItems(items ?: emptyList())
+}
+
+@BindingAdapter("app:showWhenLoading")
+fun <T> showWhenLoading(view: View, state: State<T>?) {
+    view.isVisible = state is State.Loading
+}
+
+@BindingAdapter("app:showWhenSuccess")
+fun <T> showWhenSuccess(view: View, state: State<T>?) {
+    view.isVisible = state is State.Success
+}
+
+@BindingAdapter("app:showWhenFail")
+fun <T> showWhenFail(view: View, state: State<T>?) {
+    view.isVisible = state is State.Error
+}
+
+@BindingAdapter(value = ["app:loadImage"])
+fun loadImage(view: ImageView, pathImage: String?) {
+    Glide.with(view).load("$IMAGE_DOWNLOAD_URL$pathImage")
+        .error(R.drawable.ic_baseline_person_24)
+        .placeholder(R.drawable.loading)
+        .into(view)
 }
 
 
