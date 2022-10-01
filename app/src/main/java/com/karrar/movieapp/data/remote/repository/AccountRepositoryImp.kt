bb44216fd761ac5d4f6d2ca02665a9905a8651d4
Falah Hassan <devfalahhassan@gmail.com>
2022-09-30 17:05:01 -0700

    complete AccountRepositoryImp

--- app/src/main/java/com/karrar/movieapp/data/remote/repository/AccountRepositoryImp.kt
+++ app/src/main/java/com/karrar/movieapp/data/remote/repository/AccountRepositoryImp.kt
@@ -1,60 +1,74 @@
 package com.karrar.movieapp.data.remote.repository
 
-import com.karrar.movieapp.data.remote.Either
+import com.karrar.movieapp.data.local.DataStorePreferences
+import com.karrar.movieapp.data.remote.State
 import com.karrar.movieapp.data.remote.response.login.ErrorResponse
 import com.karrar.movieapp.data.remote.service.MovieService
 import com.karrar.movieapp.utilities.DataClassParser
+import com.karrar.movieapp.utilities.DataStorePreferencesKeys
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 
 class AccountRepositoryImp @Inject constructor(
     private val movieService: MovieService,
+    private val dataStorePreferences: DataStorePreferences,
     private val dataClassParser: DataClassParser,
-) : AccountRepository{
+) : AccountRepository {
 
+
+    override fun getSessionId(): Flow<String?> {
+        return dataStorePreferences.readString(DataStorePreferencesKeys.SESSION_ID_KEY)
+    }
 
     override suspend fun loginWithUserNameANdPassword(
         userName: String,
         password: String,
-    ): Either<Boolean, String> {
+    ): Flow<State<Boolean>> {
+        return flow {
+            emit(State.Loading)
+            try {
+                val token = getRequestToken().toString()
+                val body = mapOf<String, Any>(
+                    "username" to userName,
+                    "password" to password,
+                    "request_token" to token,
+                ).toMap()
+                val validateRequestTokenWithLogin = movieService.validateRequestTokenWithLogin(body)
+                if (validateRequestTokenWithLogin.isSuccessful) {
+                    validateRequestTokenWithLogin.body()?.requestToken?.let { createSession(it) }
+                    emit(State.Success(true))
+                } else {
+                    val errorResponse =
+                        dataClassParser.parseFromJson(validateRequestTokenWithLogin.errorBody()
+                            ?.string(), ErrorResponse::class.java)
+                    emit(State.Error(errorResponse.statusMessage.toString()))
+                }
+            } catch (e: Exception) {
+                emit(State.Error(e.message.toString()))
 
-        val token = getRequestToken().toString()
-
-        val body = mapOf<String, Any>(
-            "username" to userName,
-            "password" to password,
-            "request_token" to token,
-        ).toMap()
-
-        return try {
-            val validateWithLoginToken = movieService.validateRequestTokenWithLogin(body)
-            if (validateWithLoginToken.isSuccessful) {
-                validateWithLoginToken.body()?.requestToken?.let { createSession(it) }
-                Either.Left(true)
-
-            } else {
-                val errorResponse =
-                    dataClassParser.parseFromJson(validateWithLoginToken.errorBody()?.string()
-                        .toString(), ErrorResponse::class.java)
-                Either.Right(errorResponse.statusMessage.toString())
             }
-        } catch (e: Exception) {
-            (Either.Right(e.message.toString()))
         }
-
     }
 
-    private suspend fun getRequestToken(): String? {
-        return try {
-            val tokenResponse = movieService.getRequestToken()
-            tokenResponse.body()?.requestToken
-        } catch (e: Exception) {
-            e.message
-        }
 
+    private suspend fun getRequestToken(): String? {
+        val tokenResponse = movieService.getRequestToken()
+        return tokenResponse.body()?.requestToken
     }
 
     private suspend fun createSession(requestToken: String) {
-        movieService.createSession(requestToken)
+        val sessionResponse = movieService.createSession(requestToken).body()
+        if (sessionResponse?.success == true) {
+            saveSessionId(sessionResponse.sessionId.toString())
+        }
     }
+
+
+    private suspend fun saveSessionId(sessionId: String) {
+        dataStorePreferences.writeString(DataStorePreferencesKeys.SESSION_ID_KEY, sessionId)
+    }
+
+
 }
