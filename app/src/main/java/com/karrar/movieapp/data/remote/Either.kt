3b6c834ae661af34ef063e667763c87053cdf93b
Falah Hassan <devfalahhassan@gmail.com>
2022-09-30 17:02:12 -0700

    remove Either

--- app/src/main/java/com/karrar/movieapp/data/remote/Either.kt
+++
@@ -1,6 +0,0 @@
-package com.karrar.movieapp.data.remote
-
-sealed class Either<out E, out A>{
-    data class Left<out E>(val value: E) : Either<E, Nothing>()
-    data class Right<out A>(val value: A) : Either<Nothing, A>()
-}
