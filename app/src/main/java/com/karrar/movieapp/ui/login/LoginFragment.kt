7279ca4750524ab9cc0165008edbb74592e37a2e
Falah Hassan <devfalahhassan@gmail.com>
2022-09-30 17:02:33 -0700

    complete login functionality

--- app/src/main/java/com/karrar/movieapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/karrar/movieapp/ui/login/LoginFragment.kt
@@ -1,14 +1,23 @@
 package com.karrar.movieapp.ui.login
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.karrar.movieapp.R
 import com.karrar.movieapp.databinding.FragmentLoginBinding
 import com.karrar.movieapp.ui.base.BaseFragment
+import com.karrar.movieapp.utilities.EventObserve
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>() {
     override val layoutIdFragment = R.layout.fragment_login
-    override val viewModel: LoginViewModel by viewModels ()
-
+    override val viewModel: LoginViewModel by viewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.loginEvent.observe(viewLifecycleOwner, EventObserve {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        })
+    }
 }
--- app/src/main/java/com/karrar/movieapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/karrar/movieapp/ui/login/LoginViewModel.kt
@@ -1,10 +1,13 @@
 package com.karrar.movieapp.ui.login
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.karrar.movieapp.data.remote.Either
+import com.karrar.movieapp.data.remote.State
 import com.karrar.movieapp.data.remote.repository.AccountRepository
+import com.karrar.movieapp.utilities.Event
+import com.karrar.movieapp.utilities.TextValidation
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -12,48 +15,77 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val accountRepository: AccountRepository,
+    private val textValidation: TextValidation,
 ) : ViewModel() {
 
-    private val _userName = MutableLiveData("")
+    val userName = MutableLiveData("")
+    val password = MutableLiveData("")
 
+    private val _passwordHelperText = MutableLiveData("")
+    val passwordHelperText: LiveData<String> = _passwordHelperText
+    private val _userNameHelperText = MutableLiveData("")
+    val userNameHelperText: LiveData<String> = _userNameHelperText
 
-    private val _password = MutableLiveData("")
-    fun onPasswordChanged(text: String) {
-        _password.postValue(text)
-    }
+    private val _loginRequestState = MutableLiveData<State<Boolean>>()
+    val loginRequestState: LiveData<State<Boolean>> = _loginRequestState
 
-    fun onUserNameChanged(text: String) {
-        _userName.postValue(text)
-    }
+    private val _loginEvent = MutableLiveData<Event<Boolean>>()
+    val loginEvent: LiveData<Event<Boolean>> = _loginEvent
+
 
     fun onClickLogin() {
-        viewModelScope.launch {
-            val loginState = loginWithUserNameANdPassword(_userName.value.toString(), _password.value.toString())
-            when (loginState) {
-                is Either.Left -> onLoginSuccessfully()
-                is Either.Right -> onLongError(loginState.value)
-            }
-
+        if (isValidUserName() && isValidPassword()) {
+            whenFormIsValid()
         }
     }
 
-    private suspend fun loginWithUserNameANdPassword(
-        userName: String,
-        password: String,
-    ): Either<Boolean, String> {
-        return accountRepository.loginWithUserNameANdPassword(
-            userName,
-            password
-        )
+    private fun isValidPassword(): Boolean {
+        val validPassword = textValidation.validPassword(password.value.toString())
+        _passwordHelperText.postValue(validPassword)
+        return validPassword == null
+    }
+
+    private fun isValidUserName(): Boolean {
+        val validUserName = textValidation.validFiled(userName.value.toString())
+        _userNameHelperText.postValue(validUserName)
+        return validUserName == null
+    }
+
+
+    private fun whenFormIsValid() {
+        login()
+    }
+
+    fun login() {
+        viewModelScope.launch {
+            accountRepository.loginWithUserNameANdPassword(userName.value.toString(),
+                password.value.toString()).collect {
+                when (it) {
+                    is State.Error -> onLoginError(it.message)
+                    State.Loading -> _loginRequestState.postValue(it)
+                    is State.Success -> onLoginSuccessfully()
+                }
+            }
+        }
     }
 
     private fun onLoginSuccessfully() {
+        _loginRequestState.postValue(State.Success(true))
+        _loginEvent.postValue(Event(true))
+        resetForm()
 
     }
 
-    private fun onLongError(message:String) {
+    private fun onLoginError(message: String) {
+        _loginRequestState.postValue(State.Error(message))
+        _passwordHelperText.postValue(message)
 
     }
 
+
+    private fun resetForm() {
+        userName.postValue(null)
+        password.postValue(null)
+    }
 
 }
