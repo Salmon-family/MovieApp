74ebfb19f7efbd5295915f06b7cddf01f4a6189e
Falah Hassan <devfalahhassan@gmail.com>
2022-09-30 17:03:02 -0700

    complete login xml

--- app/src/main/java/com/karrar/movieapp/utilities/DataClassParser.kt
+++ app/src/main/java/com/karrar/movieapp/utilities/DataClassParser.kt
@@ -4,7 +4,7 @@ import com.google.gson.Gson
 import javax.inject.Inject
 
 class DataClassParser @Inject constructor(val gson: Gson) {
-    inline fun <reified T> parseFromJson(json: String , classType: Class<T>): T {
+    inline fun <reified T> parseFromJson(json: String?, classType: Class<T>): T {
         return gson.fromJson(json,classType)
     }
 }
