8c804a28b1a1e327fce68f525545e3a9517ffc34
Falah Hassan <devfalahhassan@gmail.com>
2022-09-30 17:00:44 -0700

    add DataStorePreferencesKeys object

--- app/src/main/java/com/karrar/movieapp/utilities/Constants.kt
+++ app/src/main/java/com/karrar/movieapp/utilities/Constants.kt
@@ -4,3 +4,7 @@ object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val API_KEY = "84c61eff2fcefd931aff41f9d620ab21"
 }
+
+object DataStorePreferencesKeys{
+    const val SESSION_ID_KEY = "session_id"
+}
