222cfc479079b799a1347d57b440e4f53c297ff6
alkomity <197023.it.eng@student.aden-univ.net>
2022-10-02 16:45:56 +0300

    add app bar

--- app/src/main/java/com/karrar/movieapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/karrar/movieapp/ui/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.karrar.movieapp.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.karrar.movieapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.navigateUp()
+        return true
     }
 }
