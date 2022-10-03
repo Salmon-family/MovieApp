45b6c361b918b0a637d093fe04d2f72ed0a3626f
Falah Hassan <devfalahhassan@gmail.com>
2022-10-03 14:26:48 -0700

    add HomeRecyclerItem

--- app/src/main/java/com/karrar/movieapp/ui/home/HomeRecyclerItem.kt
+++ app/src/main/java/com/karrar/movieapp/ui/home/HomeRecyclerItem.kt
@@ -1,13 +1,20 @@
 package com.karrar.movieapp.ui.home
 
-import com.karrar.movieapp.domain.enums.Type
-import com.karrar.movieapp.domain.models.Actor
+import com.karrar.movieapp.R
+import com.karrar.movieapp.domain.enums.MovieType
 import com.karrar.movieapp.domain.models.Media
 import com.karrar.movieapp.domain.models.PopularMovie
 
-sealed class HomeRecyclerItem(var priority: Int) {
-    class SlideType(val items: List<PopularMovie>) : HomeRecyclerItem(0)
-    class TvShows(val items: List<Media> ) :HomeRecyclerItem(1)
-    class MoviesType(val items: List<Media>,val type: Type) : HomeRecyclerItem(2)
-    class ActorType(val items: List<Actor>) : HomeRecyclerItem(3)
+sealed class HomeRecyclerItem(var priority: Int, val layoutID: Int) {
+
+    class Slider(val items: List<PopularMovie>) : HomeRecyclerItem(0, R.layout.list_popular)
+
+    class TvShows(val items: List<Media>) : HomeRecyclerItem(1, R.layout.list_tv_shows)
+
+    class Movie(val items: List<Media>, val type: MovieType) : HomeRecyclerItem(2, R.layout.list_movie)
+
+    class AiringToday(val items: List<Media>) : HomeRecyclerItem(4, R.layout.list_airing_today)
+
+    class Actor(val items: List<com.karrar.movieapp.domain.models.Actor>) : HomeRecyclerItem(9, R.layout.list_actor)
+
 }
