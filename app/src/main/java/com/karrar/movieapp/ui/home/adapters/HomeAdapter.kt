af86c28b42031211545b9b9b7a7a8733e9f4d382
Falah Hassan <devfalahhassan@gmail.com>
2022-10-03 14:24:38 -0700

    complete home adapter

--- app/src/main/java/com/karrar/movieapp/ui/home/adapters/HomeAdapter.kt
+++ app/src/main/java/com/karrar/movieapp/ui/home/adapters/HomeAdapter.kt
@@ -1,14 +1,12 @@
 package com.karrar.movieapp.ui.home.adapters
 
+import android.annotation.SuppressLint
 import android.view.ViewGroup
-import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
 import com.karrar.movieapp.BR
-import com.karrar.movieapp.R
 import com.karrar.movieapp.ui.base.BaseAdapter
 import com.karrar.movieapp.ui.base.BaseDiffUtil
-import com.karrar.movieapp.ui.base.BaseInteractionListener
+import com.karrar.movieapp.ui.home.HomeInteractionListener
 import com.karrar.movieapp.ui.home.HomeRecyclerItem
 
 class HomeAdapter(
@@ -16,24 +14,6 @@ class HomeAdapter(
     private val listener: HomeInteractionListener,
 ) : BaseAdapter<HomeRecyclerItem>(items, listener) {
     override var layoutID: Int = 0
-
-    fun addItem(newItems: HomeRecyclerItem) {
-        val newItemsList = items.apply {
-            add(newItems)
-            sortBy {
-                it.priority
-            }
-        }
-        val diffResult = DiffUtil.calculateDiff(BaseDiffUtil(items,
-            newItemsList,
-            ::areItemsSame, ::areContentSame))
-        diffResult.dispatchUpdatesTo(this)
-        notifyDataSetChanged()
-    }
-
-    override fun areItemsSame(oldItem: HomeRecyclerItem, newItem: HomeRecyclerItem): Boolean {
-        return oldItem.priority == newItem.priority
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
         layoutID = viewType
@@ -46,22 +26,11 @@ class HomeAdapter(
 
     override fun bind(holder: ItemViewHolder, position: Int) {
         when (val currentItem = items[position]) {
-            is HomeRecyclerItem.ActorType -> {
-                holder.binding.setVariable(BR.adapterRecycler,
-                    ActorAdapter(currentItem.items, listener))
-            }
-            is HomeRecyclerItem.MoviesType -> {
-
-                holder.binding.run {
-                    setVariable(BR.adapterRecycler, MovieAdapter(currentItem.items, listener))
-                    setVariable(BR.title, currentItem.type.name)
-                }
-
-            }
-            is HomeRecyclerItem.SlideType -> {
+            is HomeRecyclerItem.Slider -> {
                 holder.binding.setVariable(BR.adapterRecycler,
                     PopularMovieAdapter(currentItem.items, listener))
             }
+
             is HomeRecyclerItem.TvShows -> {
                 holder.binding.run {
                     setVariable(BR.topRated, currentItem.items.first())
@@ -69,23 +38,48 @@ class HomeAdapter(
                     setVariable(BR.latest, currentItem.items.last())
                 }
             }
+            is HomeRecyclerItem.Movie -> {
+                holder.binding.run {
+                    setVariable(BR.adapterRecycler, MovieAdapter(currentItem.items, listener))
+                    setVariable(BR.movieType, currentItem.type)
+                }
+
+            }
+            is HomeRecyclerItem.Actor -> {
+                holder.binding.setVariable(BR.adapterRecycler, ActorAdapter(currentItem.items, listener))
+            }
+            is HomeRecyclerItem.AiringToday -> {
+                holder.binding.run {
+                    setVariable(BR.adapterRecycler,
+                        AiringTodayAdapter(currentItem.items.take(6), listener))
+                    setVariable(BR.count,currentItem.items.size)
+                }
+
+
+            }
         }
     }
 
-
-    override fun getItemViewType(position: Int) = when (items[position]) {
-        is HomeRecyclerItem.ActorType -> R.layout.list_actor
-        is HomeRecyclerItem.MoviesType -> R.layout.list_movie
-        is HomeRecyclerItem.SlideType -> R.layout.list_slider
-        is HomeRecyclerItem.TvShows -> R.layout.list_collection
+    @SuppressLint("NotifyDataSetChanged")
+    fun addItem(newItem: HomeRecyclerItem) {
+        val newItems = items.apply {
+            add(newItem)
+            sortBy {
+                it.priority
+            }
+        }
+        val diffResult = DiffUtil.calculateDiff(BaseDiffUtil(items, newItems,::areItemsSame, ::areContentSame))
+        diffResult.dispatchUpdatesTo(this)
+        notifyDataSetChanged()
     }
 
+    override fun areItemsSame(oldItem: HomeRecyclerItem, newItem: HomeRecyclerItem): Boolean {
+        return oldItem.priority == newItem.priority
+    }
+
+    override fun getItemViewType(position: Int) = items[position].layoutID
+
 
 }
-sealed class BaseViewHolder(view: ViewDataBinding) : RecyclerView.ViewHolder(view.root) {
-    class ItemViewHolder(val binding: ViewDataBinding) : BaseViewHolder(binding)
-    class MovieViewHolder(val binding: ViewDataBinding) : BaseViewHolder(binding)
-    class SlideViewHolder(val binding: ViewDataBinding) : BaseViewHolder(binding)
-}
 
-interface HomeInteractionListener : BaseInteractionListener
+
