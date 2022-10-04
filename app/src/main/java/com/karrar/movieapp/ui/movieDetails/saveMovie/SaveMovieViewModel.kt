bada81b77091d7db4c4e98c208a9af1f8dd3fcb9
Shahad161 <shahad.hamza.014@gmail.com>
2022-10-04 16:06:02 +0300

    create dialog of saving movie

--- app/src/main/java/com/karrar/movieapp/ui/movieDetails/saveMovie/SaveMovieViewModel.kt
+++ app/src/main/java/com/karrar/movieapp/ui/movieDetails/saveMovie/SaveMovieViewModel.kt
@@ -5,6 +5,7 @@ import com.karrar.movieapp.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+
 @HiltViewModel
 class SaveMovieViewModel @Inject constructor(private val movieRepository: MovieRepository
 ): BaseViewModel(){
