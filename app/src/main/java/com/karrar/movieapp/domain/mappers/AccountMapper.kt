5b5e40425713c97fd0ad96209e425058095f55a9
alkomity <197023.it.eng@student.aden-univ.net>
2022-10-05 19:01:00 +0300

    prepare account response with mapper

---
+++ app/src/main/java/com/karrar/movieapp/domain/mappers/AccountMapper.kt
@@ -0,0 +1,16 @@
+package com.karrar.movieapp.domain.mappers
+
+import com.karrar.movieapp.data.remote.response.account.AccountDto
+import com.karrar.movieapp.domain.models.Account
+import com.karrar.movieapp.utilities.Constants
+import javax.inject.Inject
+
+class AccountMapper @Inject constructor() : Mapper<AccountDto, Account> {
+    override fun map(input: AccountDto): Account {
+        return Account(
+            avatarPath = Constants.IMAGE_BASE_PATH + input.avatar?.avatarPath?.avatarPath,
+            name = input.name,
+            username = input.username
+        )
+    }
+}
---
+++ app/src/main/java/com/karrar/movieapp/domain/models/Account.kt
@@ -0,0 +1,7 @@
+package com.karrar.movieapp.domain.models
+
+data class Account (
+    val avatarPath: String? = null,
+    val name: String? = null,
+    val username: String? = null
+)
