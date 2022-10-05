6e3a4750f3646cbb46a78f8a4a5cdd1c0fcfe152
Falah Hassan <devfalahhassan@gmail.com>
2022-10-05 09:54:13 -0700

    refactor di to binds

--- app/src/main/java/com/karrar/movieapp/di/RepositoryModule.kt
+++ app/src/main/java/com/karrar/movieapp/di/RepositoryModule.kt
@@ -1,54 +1,23 @@
 package com.karrar.movieapp.di
 
-import com.karrar.movieapp.data.local.DataStorePreferences
 import com.karrar.movieapp.data.remote.repository.*
-import com.karrar.movieapp.data.remote.service.MovieService
-import com.karrar.movieapp.data.remote.service.SeriesService
-import com.karrar.movieapp.utilities.DataClassParser
-import com.karrar.movieapp.domain.mappers.ActorDetailsMapper
-import com.karrar.movieapp.domain.mappers.ActorMoviesMapper
-import com.karrar.movieapp.domain.mappers.ActorMapper
-import com.karrar.movieapp.domain.mappers.GenreMapper
-import com.karrar.movieapp.domain.mappers.MediaMapper
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+abstract class RepositoryModule {
 
-    @Singleton
-    @Provides
-    fun provideMovieRepository(
-        movieService: MovieService,
-        actorDetailsMapper: ActorDetailsMapper,
-        actorMoviesMapper: ActorMoviesMapper,
-        actorMapper: ActorMapper,
-        movieMapper: MediaMapper,
-        genreMapper: GenreMapper
-    ): MovieRepository {
-        return MovieRepositoryImp(movieService, actorDetailsMapper, actorMoviesMapper, actorMapper, genreMapper, movieMapper)
-    }
 
-    @Provides
-    @Singleton
-    fun provideSeriesRepository(
-        seriesService: SeriesService,
-        seriesMapper: MediaMapper
-    ): SeriesRepository {
-        return SeriesRepositoryImp(seriesService, seriesMapper)
-    }
+    @Binds
+    abstract fun bindMovieRepository(movieRepositoryImp: MovieRepositoryImp): MovieRepository
 
-    @Singleton
-    @Provides
-    fun provideAccountRepository(
-        movieService: MovieService,
-        dataStorePreferences: DataStorePreferences,
-        dataClassParser: DataClassParser,
-    ): AccountRepository {
-        return AccountRepositoryImp(movieService, dataStorePreferences, dataClassParser)
-    }
+    @Binds
+    abstract fun bindSeriesRepository(seriesRepositoryImp: SeriesRepositoryImp): SeriesRepository
+
+    @Binds
+    abstract fun bindAccountRepository(accountRepositoryImp: AccountRepositoryImp): AccountRepository
+
 }
