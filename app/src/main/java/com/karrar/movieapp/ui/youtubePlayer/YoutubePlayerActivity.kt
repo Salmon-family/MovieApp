88b226c88b42d891a84692211e89d55c8f19d07f
amnah.a <amnah44ali@gmail.com>
2022-10-08 00:26:05 +0300

    hide action bar in youtube activity

--- app/src/main/java/com/karrar/movieapp/ui/youtubePlayer/YoutubePlayerActivity.kt
+++ app/src/main/java/com/karrar/movieapp/ui/youtubePlayer/YoutubePlayerActivity.kt
@@ -2,6 +2,7 @@ package com.karrar.movieapp.ui.youtubePlayer
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.karrar.movieapp.R
 import com.karrar.movieapp.databinding.ActivityYoutubePlayerBinding
 import com.karrar.movieapp.ui.base.BaseActivity
@@ -15,5 +16,7 @@ class YoutubePlayerActivity : BaseActivity<ActivityYoutubePlayerBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
     }
 }
