b773b48e6b421db15ce51ff9218a75d1eb154bf4
omar ezzdeen <omar.izziddeen@gmail.com>
2022-10-07 03:03:11 +0300

    fix branch develop

--- app/src/main/java/com/karrar/movieapp/domain/mappers/MediaMapper.kt
+++ app/src/main/java/com/karrar/movieapp/domain/mappers/MediaMapper.kt
@@ -3,13 +3,14 @@ package com.karrar.movieapp.domain.mappers
 import com.karrar.movieapp.BuildConfig
 import com.karrar.movieapp.data.remote.response.MovieDto
 import com.karrar.movieapp.domain.models.Media
+import com.karrar.movieapp.utilities.Constants
 import javax.inject.Inject
 
 class MediaMapper @Inject constructor() : Mapper<MovieDto, Media> {
     override fun map(input: MovieDto): Media {
         return Media(
             input.id ?: 0,
-            BuildConfig.IMAGE_BASE_PATH + input.posterPath
+            Constants.IMAGE_BASE_PATH + input.posterPath
         )
     }
 }
--- app/src/main/java/com/karrar/movieapp/domain/mappers/SeriesMapper.kt
+++ app/src/main/java/com/karrar/movieapp/domain/mappers/SeriesMapper.kt
@@ -3,6 +3,7 @@ package com.karrar.movieapp.domain.mappers
 import com.karrar.movieapp.BuildConfig
 import com.karrar.movieapp.data.remote.response.SeriesDto
 import com.karrar.movieapp.domain.models.MediaInfo
+import com.karrar.movieapp.utilities.Constants
 import javax.inject.Inject
 
 class SeriesMapper @Inject constructor() : Mapper<SeriesDto, MediaInfo> {
@@ -13,7 +14,7 @@ class SeriesMapper @Inject constructor() : Mapper<SeriesDto, MediaInfo> {
             name = input.originalName,
             releaseDate = input.firstAirDate?.substringBefore('-'),
             rate = input.voteAverage?.toFloat(),
-            imagePath = BuildConfig.IMAGE_BASE_PATH + input.backdropPath,
+            imagePath = Constants.IMAGE_BASE_PATH + input.backdropPath,
             profileImage = null
         )
     }
