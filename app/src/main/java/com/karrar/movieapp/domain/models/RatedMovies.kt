eaa75d1377397c8906b18af4bfad2a72e09f6644
alkomity <197023.it.eng@student.aden-univ.net>
2022-10-08 23:42:38 +0300

    make rated movies not nullable

--- app/src/main/java/com/karrar/movieapp/domain/models/RatedMovies.kt
+++ app/src/main/java/com/karrar/movieapp/domain/models/RatedMovies.kt
@@ -1,9 +1,9 @@
 package com.karrar.movieapp.domain.models
 
 data class RatedMovies(
-    val id: Int?,
-    val title: String?,
-    val posterPath: String?,
-    val rating: Int?,
-    val releaseDate: String?,
+    val id: Int,
+    val title: String,
+    val posterPath: String,
+    val rating: Int,
+    val releaseDate: String,
 )
