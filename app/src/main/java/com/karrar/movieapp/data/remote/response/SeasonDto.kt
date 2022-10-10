b006f5ca3b46dbf7eeb371adea2f1eaa804a2964
Mustafa Ahmed <mstfyjmalalden@gmail.com>
2022-10-10 22:23:00 +0300

    add tvShowDetail and episodes responses

---
+++ app/src/main/java/com/karrar/movieapp/data/remote/response/SeasonDto.kt
@@ -0,0 +1,26 @@
+package com.karrar.movieapp.data.remote.response
+
+
+import com.google.gson.annotations.SerializedName
+import com.karrar.movieapp.data.remote.response.episode.EpisodeDto
+
+data class SeasonDto(
+    @SerializedName("air_date")
+    val airDate: String? = null,
+    @SerializedName("episode_count")
+    val episodeCount: Int? = null,
+    @SerializedName("episodes")
+    val episodes: List<EpisodeDto?>? = null,
+    @SerializedName("_id")
+    val idString: String? = null,
+    @SerializedName("id")
+    val id: Int? = null,
+    @SerializedName("name")
+    val name: String? = null,
+    @SerializedName("overview")
+    val overview: String? = null,
+    @SerializedName("poster_path")
+    val posterPath: String? = null,
+    @SerializedName("season_number")
+    val seasonNumber: Int? = null
+)
---
+++ app/src/main/java/com/karrar/movieapp/data/remote/response/tvShow/ProductionCountryDto.kt
@@ -0,0 +1,11 @@
+package com.karrar.movieapp.data.remote.response.tvShow
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductionCountryDto(
+    @SerializedName("iso_3166_1")
+    val iso31661: String? = null,
+    @SerializedName("name")
+    val name: String? = null
+)
