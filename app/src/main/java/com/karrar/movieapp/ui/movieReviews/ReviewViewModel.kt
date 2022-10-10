eb3ad655473854324c7df5e776ddfc56246e33ff
Mustafa Ahmed <mstfyjmalalden@gmail.com>
2022-10-10 22:45:40 +0300

    add enum to reviewViewModel

--- app/src/main/java/com/karrar/movieapp/ui/movieReviews/ReviewViewModel.kt
+++ app/src/main/java/com/karrar/movieapp/ui/movieReviews/ReviewViewModel.kt
@@ -3,6 +3,8 @@ package com.karrar.movieapp.ui.movieReviews
 import androidx.lifecycle.*
 import com.karrar.movieapp.data.remote.State
 import com.karrar.movieapp.data.repository.MovieRepository
+import com.karrar.movieapp.data.repository.SeriesRepository
+import com.karrar.movieapp.domain.enums.MediaType
 import com.karrar.movieapp.domain.models.Review
 import com.karrar.movieapp.ui.base.BaseInteractionListener
 import com.karrar.movieapp.ui.base.BaseViewModel
@@ -11,15 +13,30 @@ import javax.inject.Inject
 
 
 @HiltViewModel
-class ReviewViewModel @Inject constructor(private val movieRepository: MovieRepository
-): BaseViewModel(), BaseInteractionListener {
+class ReviewViewModel @Inject constructor(
+    private val movieRepository: MovieRepository,
+    private val seriesRepository: SeriesRepository,
+    state: SavedStateHandle
+) : BaseViewModel(), BaseInteractionListener {
+
+    val args = ReviewFragmentArgs.fromSavedStateHandle(state)
 
     private var _movieReviews = MutableLiveData<State<List<Review>>>()
     val movieReviews : LiveData<State<List<Review>>> = _movieReviews
 
-    fun getAllReviews(movie_id:Int){
-        collectResponse(movieRepository.getMovieReviews(movie_id)){
-            _movieReviews.postValue(it)
+
+    init {
+        getAllReviews(args.mediaId)
+    }
+
+    private fun getAllReviews(mediaId: Int) {
+        when (args.type) {
+            MediaType.MOVIE -> collectResponse(movieRepository.getMovieReviews(mediaId)) {
+                _movieReviews.postValue(it)
+            }
+            MediaType.TV_SHOW -> collectResponse(seriesRepository.getTvShowReviews(mediaId)) {
+                _movieReviews.postValue(it)
+            }
         }
     }
 
