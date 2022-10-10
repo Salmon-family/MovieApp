2d00aaaa03ab7ede921e3dedd5af955c333d5e45
Mustafa Ahmed <mstfyjmalalden@gmail.com>
2022-10-10 22:35:34 +0300

    implement seasons list

--- app/src/main/java/com/karrar/movieapp/utilities/Constants.kt
+++ app/src/main/java/com/karrar/movieapp/utilities/Constants.kt
@@ -2,7 +2,7 @@ package com.karrar.movieapp.utilities
 
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val IMAGE_BASE_PATH = "https://image.tmdb.org/t/p/w500"
+    const val IMAGE_BASE_PATH = "https://image.tmdb.org/t/p/w780"
     const val TMDB_SIGNUP_URL="https://www.themoviedb.org/signup"
     const val MAX_NUMBER_AIRING_TODAY = 6
     const val MYSTERY_ID = 9648
