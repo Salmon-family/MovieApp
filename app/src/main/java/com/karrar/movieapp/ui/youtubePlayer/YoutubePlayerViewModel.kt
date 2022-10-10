bd5c70c14bba858e86d68ba441e0ea545299ce1c
Mustafa Ahmed <mstfyjmalalden@gmail.com>
2022-10-10 22:45:58 +0300

    add enum to youtubePlayerViewModel

--- app/src/main/java/com/karrar/movieapp/ui/youtubePlayer/YoutubePlayerViewModel.kt
+++ app/src/main/java/com/karrar/movieapp/ui/youtubePlayer/YoutubePlayerViewModel.kt
@@ -3,6 +3,8 @@ package com.karrar.movieapp.ui.youtubePlayer
 import androidx.lifecycle.*
 import com.karrar.movieapp.data.remote.State
 import com.karrar.movieapp.data.repository.MovieRepository
+import com.karrar.movieapp.data.repository.SeriesRepository
+import com.karrar.movieapp.domain.enums.MediaType
 import com.karrar.movieapp.domain.models.Trailer
 import com.karrar.movieapp.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,6 +14,7 @@ import javax.inject.Inject
 @HiltViewModel
 class YoutubePlayerViewModel @Inject constructor(
     private val movieRepository: MovieRepository,
+    private val seriesRepository: SeriesRepository,
     state: SavedStateHandle
 ):BaseViewModel() {
 
@@ -24,9 +27,14 @@ class YoutubePlayerViewModel @Inject constructor(
         getMovieTrailer(args.movieId)
     }
 
-    private fun getMovieTrailer(movie_id: Int){
-        collectResponse(movieRepository.getMovieTrailer(movie_id)) {
-            _movieTrailer.postValue(it)
+    private fun getMovieTrailer(movie_id: Int) {
+        when (args.type) {
+            MediaType.MOVIE -> collectResponse(movieRepository.getMovieTrailer(movie_id)) {
+                _movieTrailer.postValue(it)
+            }
+            MediaType.TV_SHOW -> collectResponse(seriesRepository.getTvShowTrailer(movie_id)) {
+                _movieTrailer.postValue(it)
+            }
         }
     }
 
