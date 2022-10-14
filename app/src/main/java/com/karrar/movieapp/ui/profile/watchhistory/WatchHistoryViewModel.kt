9e612a1f6219e21bf477f888b9f2e4338a322fa4
alkomity <197023.it.eng@student.aden-univ.net>
2022-10-14 14:49:55 +0300

    make watch history with flow

--- app/src/main/java/com/karrar/movieapp/ui/profile/watchhistory/WatchHistoryViewModel.kt
+++ app/src/main/java/com/karrar/movieapp/ui/profile/watchhistory/WatchHistoryViewModel.kt
@@ -2,15 +2,13 @@ package com.karrar.movieapp.ui.profile.watchhistory
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.karrar.movieapp.data.local.database.entity.WatchHistoryEntity
+import androidx.lifecycle.asLiveData
 import com.karrar.movieapp.data.repository.MovieRepository
 import com.karrar.movieapp.utilities.Constants
 import com.karrar.movieapp.utilities.Event
 import com.karrar.movieapp.utilities.postEvent
 import com.karrar.movieapp.utilities.toLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -24,11 +22,10 @@ class WatchHistoryViewModel @Inject constructor(
     private val _clickTVShowEvent = MutableLiveData<Event<Int>>()
     val clickTVShowEvent = _clickTVShowEvent.toLiveData()
 
-    private val _watchHistory = MutableLiveData<List<WatchHistoryEntity>>()
-    val watchHistory = _watchHistory.toLiveData()
+    val watchHistory = movieRepository.getAllWatchedMovies().asLiveData()
 
     override fun onClickMovie(mediaID: Int) {
-        _watchHistory.value?.let { it ->
+        watchHistory.value?.let { it ->
             val item = it.find { it.id == mediaID }
             item?.let {
                 if (it.mediaType == Constants.MOVIE) {
@@ -37,19 +34,6 @@ class WatchHistoryViewModel @Inject constructor(
                     _clickTVShowEvent.postEvent(mediaID)
                 }
             }
-        }
-    }
-
-    init {
-        viewModelScope.launch {
-            val result = movieRepository.getAllWatchedMovies()
-            _watchHistory.postValue(result)
-        }
-    }
-
-    fun clearHistory() {
-        viewModelScope.launch {
-            movieRepository.clearWatchHistory()
         }
     }
 }
