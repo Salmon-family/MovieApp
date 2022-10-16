3799f051377369be8ab41c0b345e840c3984a469
omar ezzdeen <omar.izziddeen@gmail.com>
2022-10-16 20:54:15 +0300

    fix bottom navigation bar

--- app/src/main/java/com/karrar/movieapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/karrar/movieapp/ui/main/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.karrar.movieapp.R
 import com.karrar.movieapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,8 +19,6 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-    private lateinit var navHostFragment: NavHostFragment
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,43 +29,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        setNavigationController()
-
-        setBottomNavigationVisibility()
     }
 
-    private fun setNavigationController() {
-        navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+    override fun onResume() {
+        super.onResume()
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
 
-        binding.bottomNavigation.setOnItemSelectedListener { item ->
-            NavigationUI.onNavDestinationSelected(item, navController)
-            navController.popBackStack(item.itemId, inclusive = false)
-            true
-        }
-    }
-
-    private fun setBottomNavigationVisibility(){
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            binding.bottomNavigation.isVisible = destination.id != R.id.loginFragment
-        }
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
-    override fun onResume() {
-        super.onResume()
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment,
-                R.id.exploringFragment,
-                R.id.myListFragment,
-                R.id.profileFragment,
-
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        findNavController(R.id.nav_host_fragment).navigateUp()
+        return true
     }
 }
