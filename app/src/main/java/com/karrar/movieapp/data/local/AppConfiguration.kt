806fc01f78468a9c720de52d8e61a9ad23b679bb
Falah Hassan <devfalahhassan@gmail.com>
2022-10-21 16:38:55 -0700

    add save request date methods

--- app/src/main/java/com/karrar/movieapp/data/local/AppConfiguration.kt
+++ app/src/main/java/com/karrar/movieapp/data/local/AppConfiguration.kt
@@ -9,14 +9,32 @@ interface AppConfiguration {
     fun readString(key: String): Flow<String?>
 
     suspend fun writeString(key: String, value: String)
+
+    suspend fun saveRequestDate(value: Long)
+
+    suspend fun getRequestDate(): Long?
 }
 
-class AppConfigurator @Inject constructor(private val dataStorePreferences: DataStorePreferences) :AppConfiguration{
+class AppConfigurator @Inject constructor(private val dataStorePreferences: DataStorePreferences) :
+    AppConfiguration {
     override fun readString(key: String): Flow<String?> {
         return dataStorePreferences.readString(key)
     }
 
     override suspend fun writeString(key: String, value: String) {
-        dataStorePreferences.writeString(key,value)
+        dataStorePreferences.writeString(key, value)
+    }
+
+    override suspend fun saveRequestDate(value: Long) {
+        dataStorePreferences.writeLong(REQUEST_DATE_KEY, value)
+    }
+
+    override suspend fun getRequestDate(): Long? {
+        return dataStorePreferences.readLong(REQUEST_DATE_KEY)
+    }
+
+    companion object DataStorePreferencesKeys {
+        const val SESSION_ID_KEY = "session_id"
+        const val REQUEST_DATE_KEY = "request_date"
     }
 }
--- app/src/main/java/com/karrar/movieapp/data/local/DataStorePreferences.kt
+++ app/src/main/java/com/karrar/movieapp/data/local/DataStorePreferences.kt
@@ -4,25 +4,25 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
 
 class DataStorePreferences (context: Context) {
     private val Context.preferencesDataStore: DataStore<androidx.datastore.preferences.core.Preferences> by preferencesDataStore(PREFERENCES_FILE_NAME)
     private val prefDataStore = context.preferencesDataStore
 
-    suspend fun writeBoolean(key: String, value: Boolean) {
+    suspend fun writeLong(key: String, value: Long) {
         prefDataStore.edit { preferences ->
-            preferences[booleanPreferencesKey(key)] = value
+            preferences[longPreferencesKey(key)] = value
         }
     }
 
-    fun readBoolean(key: String): Flow<Boolean?> {
-        return prefDataStore.data.map { preferences ->
-            preferences[booleanPreferencesKey(key)]
-        }
+    suspend fun readLong(key: String): Long? {
+        return prefDataStore.data.firstOrNull()?.get(longPreferencesKey(key))
     }
 
     suspend fun writeString(key: String, value: String){
