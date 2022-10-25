3e95b010755b6a28832aed5bddb271c8f415339a
alkomity <197023.it.eng@student.aden-univ.net>
2022-10-25 21:06:26 +0300

    create ui state for actor details

---
+++ app/src/main/java/com/karrar/movieapp/ui/actorDetails/ActorDetailsUIState.kt
@@ -0,0 +1,14 @@
+package com.karrar.movieapp.ui.actorDetails
+
+data class ActorDetailsUIState(
+    val name: String = "",
+    val imageUrl: String = "",
+    val gender: String = "",
+    val birthday: String = "",
+    val placeOfBirth: String = "",
+    val knownFor: String = "",
+    val biography: String = "",
+    val isLoading: Boolean = false,
+    val error: Error = Error(""),
+    val actorMovies: List<ActorMoviesUIState> = emptyList()
+)
---
+++ app/src/main/java/com/karrar/movieapp/ui/actorDetails/ActorMoviesUIState.kt
@@ -0,0 +1,6 @@
+package com.karrar.movieapp.ui.actorDetails
+
+data class ActorMoviesUIState(
+    val id: Int = 0,
+    val imageUrl: String = ""
+)
