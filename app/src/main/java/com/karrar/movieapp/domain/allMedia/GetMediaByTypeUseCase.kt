64cdf27d0d65cf8b459f75bc710cc73cdcc31cba
Falah Hassan <devfalahhassan@gmail.com>
2022-10-25 00:17:04 -0700

    refactor use case

--- app/src/main/java/com/karrar/movieapp/domain/allMedia/GetMediaByTypeUseCase.kt
+++ app/src/main/java/com/karrar/movieapp/domain/allMedia/GetMediaByTypeUseCase.kt
@@ -1,40 +1,65 @@
 package com.karrar.movieapp.domain.allMedia
 
 import androidx.paging.Pager
-import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.karrar.movieapp.data.repository.MovieRepository
+import com.karrar.movieapp.data.repository.SeriesRepository
 import com.karrar.movieapp.domain.enums.AllMediaType
-import com.karrar.movieapp.domain.mappers.MediaDataSourceContainer
+import com.karrar.movieapp.domain.mappers.MovieMappersContainer
+import com.karrar.movieapp.domain.mappers.SeriesMapperContainer
+import com.karrar.movieapp.domain.mappers.movie.MovieMapper
+import com.karrar.movieapp.domain.mappers.series.TVShowMapper
 import com.karrar.movieapp.domain.models.Media
-import com.karrar.movieapp.utilities.Constants
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetMediaByTypeUseCase @Inject constructor(
-    private val mediaDataSourceContainer: MediaDataSourceContainer,
+    private val movieRepository: MovieRepository,
+    private val seriesRepository: SeriesRepository,
+    private val movieMapper: MovieMapper,
+    private val tvShowMapper: TVShowMapper,
 ) {
-    private val config = PagingConfig(pageSize = 100, prefetchDistance = 5, enablePlaceholders = false)
 
-    operator fun invoke(type:AllMediaType) : Pager<Int, Media> {
-        val pagingSourceFactory = when (type) {
-            AllMediaType.ON_THE_AIR, AllMediaType.LATEST -> mediaDataSourceContainer.onTheAirTvShowDataSource
-            AllMediaType.AIRING_TODAY -> mediaDataSourceContainer.airingTodayTvShowDataSource
-            AllMediaType.POPULAR -> mediaDataSourceContainer.popularTvShowDataSource
-            AllMediaType.TOP_RATED -> mediaDataSourceContainer.topRatedTvShowDataSource
-            AllMediaType.TRENDING -> mediaDataSourceContainer.trendingMovieDataSource
-            AllMediaType.NOW_STREAMING -> mediaDataSourceContainer.nowStreamingMovieMovieDataSource
-            AllMediaType.UPCOMING -> mediaDataSourceContainer.upcomingMovieMovieDataSource
-            AllMediaType.MYSTERY -> {
-                val dataSource = mediaDataSourceContainer.movieGenreShowDataSource
-                dataSource.setGenre(Constants.MYSTERY_ID, Constants.MOVIE_CATEGORIES_ID)
-                dataSource
+    operator fun invoke(type: AllMediaType) : Flow<PagingData<Media>>{
+        return when(type){
+            AllMediaType.NON -> {throw Exception("")}
+            AllMediaType.LATEST ,
+            AllMediaType.AIRING_TODAY ->{
+                wrapper({seriesRepository.getAiringTodayTvShowPager()},tvShowMapper::map)
             }
-            AllMediaType.NON,
-            AllMediaType.ADVENTURE -> {
-                val dataSource = mediaDataSourceContainer.movieGenreShowDataSource
-                dataSource.setGenre(Constants.ADVENTURE_ID, Constants.MOVIE_CATEGORIES_ID)
-                dataSource
+            AllMediaType.ON_THE_AIR ->{
+                    wrapper({seriesRepository.getTopRatedTvShowPager()},tvShowMapper::map)
             }
-
+            AllMediaType.POPULAR ->{
+                wrapper({seriesRepository.getPopularTvShowPager()},tvShowMapper::map)
+            }
+            AllMediaType.TOP_RATED ->{
+                wrapper({seriesRepository.getTopRatedTvShowPager()},tvShowMapper::map)
+            }
+            AllMediaType.TRENDING -> {
+                wrapper({movieRepository.getTrendingMoviesPager()},movieMapper::map)
+            }
+            AllMediaType.NOW_STREAMING -> {
+                wrapper({movieRepository.getNowPlayingMoviesPager()},movieMapper::map)
+            }
+            AllMediaType.UPCOMING -> {
+                wrapper({movieRepository.getUpcomingMoviesPager()},movieMapper::map)
         }
-        return Pager(config = config, pagingSourceFactory = { pagingSourceFactory })
+            AllMediaType.MYSTERY -> {
+                wrapper({movieRepository.getMysteryMoviesPager()},movieMapper::map)
+        }
+            AllMediaType.ADVENTURE -> {
+                wrapper({movieRepository.getAdventureMoviesPager()},movieMapper::map)
+            }
+        }
+    }
+
+    private fun <T:Any>wrapper(
+        data :()-> Pager<Int,T>,
+        mapper : (T) -> Media
+    ) :Flow<PagingData<Media>>{
+        return data().flow.map { pager -> pager.map { mapper(it) } }
     }
 }
