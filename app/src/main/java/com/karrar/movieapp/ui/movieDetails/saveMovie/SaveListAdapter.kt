d87fb6fdf6405d40d60b1ccd14e6c1fcaaab0a51
Nada Feteiha <nada.feteiha@gmail.com>
2022-10-26 01:59:43 -0700

    add saveMovieView model and use case

--- app/src/main/java/com/karrar/movieapp/ui/movieDetails/saveMovie/SaveListAdapter.kt
+++ app/src/main/java/com/karrar/movieapp/ui/movieDetails/saveMovie/SaveListAdapter.kt
@@ -1,16 +1,15 @@
 package com.karrar.movieapp.ui.movieDetails.saveMovie
 
 import com.karrar.movieapp.R
-import com.karrar.movieapp.data.remote.response.CreatedListDto
-import com.karrar.movieapp.domain.models.CreatedList
 import com.karrar.movieapp.ui.base.BaseAdapter
 import com.karrar.movieapp.ui.base.BaseInteractionListener
 
-class SaveListAdapter(items: List<CreatedList>, listener: SaveListInteractionListener
-): BaseAdapter<CreatedList>(items, listener) {
+class SaveListAdapter(
+    items: List<MySavedListUIState>, listener: SaveListInteractionListener
+) : BaseAdapter<MySavedListUIState>(items, listener) {
     override val layoutID: Int = R.layout.item_list
 }
 
 interface SaveListInteractionListener : BaseInteractionListener {
-    fun onClickSaveList(list: CreatedList)
+    fun onClickSaveList(listID: Int)
 }
