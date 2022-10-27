3ee387547203e5c2c21d5d96ab489f72684dcf18
alkomity <197023.it.eng@student.aden-univ.net>
2022-10-27 23:37:01 +0300

    create get details and check login use cases

---
+++ app/src/main/java/com/karrar/movieapp/domain/usecase/CheckIfLoggedInUseCase.kt
@@ -0,0 +1,10 @@
+package com.karrar.movieapp.domain.usecase
+
+import com.karrar.movieapp.data.repository.AccountRepository
+import javax.inject.Inject
+
+class CheckIfLoggedInUseCase @Inject constructor(private val accountRepository: AccountRepository) {
+    operator fun invoke() : Boolean{
+        return accountRepository.getSessionId().toString().isNotEmpty()
+    }
+}
