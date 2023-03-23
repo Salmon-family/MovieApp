fe2fdb20701ccedd4fb9ab7aa7781c19d17ff4e5
karrar mohammed <karrar.m.habeeb@gmail.com>
2023-03-23 18:25:49 +0300

    fix text color and size

--- app/src/main/java/com/karrar/movieapp/ui/composable/MediaHorizontalItem.kt
+++ app/src/main/java/com/karrar/movieapp/ui/composable/MediaHorizontalItem.kt
@@ -4,53 +4,53 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.karrar.movieapp.R
 import com.karrar.movieapp.ui.search.mediaSearchUIState.MediaUIState
-import com.karrar.movieapp.ui.ui.theme.HorizontalMediaCardBackground
-import com.karrar.movieapp.ui.ui.theme.Yellow
+import com.karrar.movieapp.ui.theme.Typography
+import com.karrar.movieapp.ui.theme.Yellow
 
 @Composable
 fun MediaHorizontalItem(
     mediaUIState: MediaUIState,
     modifier: Modifier = Modifier,
 ) {
-    //fix text font, size and color
     Row(
         modifier = modifier
             .padding(16.dp)
             .fillMaxWidth()
-            .height(80.dp)
+            .wrapContentHeight()
             .clip(RoundedCornerShape(8.dp))
-            .background(HorizontalMediaCardBackground)
+            .background(MaterialTheme.colors.surface)
     ) {
         Image(
             painter = painterResource(id = R.drawable.chris_evans),
             contentDescription = null,
             modifier = Modifier
                 .width(142.dp)
-                .fillMaxHeight(),
+                .height(80.dp),
             contentScale = ContentScale.Crop
         )
 
         Column(
             modifier = Modifier
-                .fillMaxHeight()
+                .height(80.dp)
                 .padding(horizontal = 16.dp, vertical = 8.dp),
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
                 text = mediaUIState.mediaName,
-                color = Color.White
+                color = MaterialTheme.colors.primaryVariant,
+                style = Typography.body2
             )
 
             Row(modifier = Modifier.fillMaxWidth(),
@@ -65,6 +65,7 @@ fun MediaHorizontalItem(
                 Text(
                     text = mediaUIState.mediaVoteAverage.toString(),
                     color = Yellow,
+                    style = Typography.body1,
                     modifier = Modifier
                         .weight(1f)
                         .padding(start = 8.dp)
@@ -72,7 +73,8 @@ fun MediaHorizontalItem(
 
                 Text(
                     text = mediaUIState.mediaReleaseDate,
-                    color = Color.White,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = Typography.body2
                 )
             }
         }
--- app/src/main/java/com/karrar/movieapp/ui/main/StartActivity.kt
+++ app/src/main/java/com/karrar/movieapp/ui/main/StartActivity.kt
@@ -6,13 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.karrar.movieapp.ui.composable.MediaHorizontalItem
-import com.karrar.movieapp.ui.search.mediaSearchUIState.MediaUIState
-import com.karrar.movieapp.ui.ui.theme.MovieAppTheme
+import com.karrar.movieapp.ui.theme.MovieAppTheme
 
 class StartActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,30 +19,9 @@ class StartActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MediaHorizontalItem(mediaUIState = MediaUIState(
-                        23,
-                        "Avengers",
-                        "imageUrl",
-                        "imageUrl",
-                        9.8f,
-                        "2012"
-                    )
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
     }
 }
